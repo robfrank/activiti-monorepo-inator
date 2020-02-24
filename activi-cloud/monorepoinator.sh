28fb4343bb948d755ed87dbfe5de1a98e20a3342
robfrank <roberto.franchini@alfresco.com>
2020-02-24 17:07:45 +0100

    review of travis, patches

--- activi-cloud/monorepoinator.sh
+++ activi-cloud/monorepoinator.sh
@@ -2,14 +2,14 @@
 
 script_dir="$(cd "$( dirname "$0" )" && pwd)"
 
-monorepo_dir=$script_dir/../../activiti-cloud-monorepo-dest-9
+monorepo_dir=$script_dir/../../activiti-cloud-final
 git_base_url="git@github.com:Activiti"
 git_branch="develop"
-git_dest_branch="develop-mono"
+git_dest_branch="develop"
 
 shopt -s extglob
 
-repositories="activiti-cloud-build|activiti-cloud-api|activiti-cloud-service-common|activiti-cloud-runtime-bundle-service|activiti-cloud-messages-service|activiti-cloud-query-service|activiti-cloud-audit-service|activiti-cloud-modeling-service|activiti-cloud-notifications-graphql-service|activiti-cloud-acceptance-tests"
+repositories="activiti-cloud-build|activiti-cloud-api|activiti-cloud-service-common|activiti-cloud-runtime-bundle-service|activiti-cloud-messages-service|activiti-cloud-query-service|activiti-cloud-audit-service|activiti-cloud-modeling-service|activiti-cloud-notifications-graphql-service|activiti-cloud-acceptance-tests|activiti-cloud-connectors"
 
 files_to_delete=".dependabot,.dockerignore,.editorconfig,.helmignore,.mergify.yml,.travis.yml,.updatebot.yml,.gitignore,.codecov.yml"
 
@@ -66,35 +66,62 @@ find . -name pom.xml -exec dos2unix {} \;
 git commit -m "fix CRLF on poms" .
 echo "---> fix CRLF on poms"
 
-echo "fix child versions"
-mvn versions:update-child-modules -DallowSnapshots=true -DgenerateBackupPoms=false
-mvn versions:commit
-git commit -m "fix child versions" .
-
-echo "update pom properties"
-mvn versions:update-properties -DgenerateBackupPoms=false -Dexcludes=org.activiti.cloud.build:activiti-cloud-dependencies-parent
-mvn versions:commit
-git commit -m "update pom properties" .
-echo "---> update pom properties"
-
 echo "apply patch to disable enforcer: "
 git apply -v $script_dir/disable-enforcer.patch
 git commit -m "disbaled enforcer with patch" .
+echo "--->apply patch to disable enforcer: "
 
+
+echo "fix child versions"
+mvn -N versions:update-child-modules -DallowSnapshots=true -DgenerateBackupPoms=false
+#mvn versions:commit
+git commit -m "fix child versions" .
+echo "---> fix child versions"
+#
 echo "apply patch to fix parent: "
 git apply -v $script_dir/fix-parent.patch
 git commit -m "fix pom parent with patch" .
+echo "---> apply patch to fix parent: "
+
+echo "update pom properties"
+mvn versions:update-properties -DgenerateBackupPoms=false -Dexcludes=org.springframework:*
+#mvn versions:commit
+git commit -m "update pom properties" .
+echo "---> update pom properties"
 
 echo "apply patch to fix poms: revert versions"
 git apply -v $script_dir/revert-versions.patch
 git commit -m "fix pom versions with patch" .
+echo "---> apply patch to fix poms: revert versions"
+
+echo "remove dependencies-tests"
+gfind -path "*/dependencies-tests" -exec rm -rf {} \;
+echo "--->remove dependencies-tests"
+
+echo "apply remove dependencies-test: "
+git apply -v $script_dir/remove-dependencies-test.patch
+git commit -m "remove dependencies-test with patch" .
+echo "--->apply patch remove dependencies-test: "
+
+echo "apply patch to set log Off"
+git apply -v $script_dir/log-off.patch
+git commit -m "set log off" .
+echo "---> apply patch to set log Off"
+
 
 
 echo "copying build configuration files"
 cp -a $script_dir/conf-files/.github $monorepo_dir/
 cp -a $script_dir/conf-files/.*.yml $monorepo_dir/
+cp -a $script_dir/conf-files/.gitignore $monorepo_dir/
+cp -a $script_dir/conf-files/.editorconfig $monorepo_dir/
 cp -a $script_dir/conf-files/Jenkinsfile $monorepo_dir/
 cp -a $script_dir/conf-files/Makefile $monorepo_dir/
+
+echo "add logback configuration"
+gfind $monorepo_dir -path "*/src/test" -exec mkdir -p {}/resources \;
+gfind $monorepo_dir -path "*/src/test" -exec cp -a $script_dir/conf-files/logback-test.xml {}/resources/ \;
+
 git add .
 git commit -m "add build configuration files" .
 
